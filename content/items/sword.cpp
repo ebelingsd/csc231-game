13c9ff3ffa6d7b278a55e18569a83723f7b975d9
silas <silasebeling8@gmail.com>
2024-04-28 19:34:16 -0700

    add noise to sword

--- content/items/sword.cpp
+++ content/items/sword.cpp
@@ -1,14 +1,19 @@
 #include "sword.h"
+
+#include "animation.h"
 #include "engine.h"
+#include "entity.h"
 #include "hit.h"
 #include "thrust.h"
-#include "entity.h"
-
+#include "sound.h"
 
 Sword::Sword(int damage)
     :Item{"sword"}, damage{damage} {}
 
 void Sword::use(Engine& engine, Entity& attacker, Entity& defender) {
-    engine.events.create_event<Thrust>(sprite, attacker.get_direction());
-    engine.events.create_event<Hit>(defender, damage);
+    auto thrust = engine.events.create_event<Thrust>(sprite, attacker.get_direction());
+    std::shared_ptr<Event> lightning = std::make_shared<Animation>("lightning", defender.get_position());
+    lightning->add_next(Hit{defender, damage});
+    engine.events.create_event<Sound>("thunder");
+    thrust->add_next(lightning);     //engine.events.create_event<Hit>(defender, damage);
 }
