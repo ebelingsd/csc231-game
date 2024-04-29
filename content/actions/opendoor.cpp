a5ef60754dc03e057477229e8baa1cd06f88d2ba
silas <silasebeling8@gmail.com>
2024-04-28 19:34:28 -0700

    add noise to opendoor

--- content/actions/opendoor.cpp
+++ content/actions/opendoor.cpp
@@ -3,12 +3,14 @@
 #include "door.h"
 #include "engine.h"
 #include "updatefov.h"
+#include "sound.h"
 
 
 
 Result OpenDoor::perform(Engine& engine, std::shared_ptr<Entity> entity) { //maybe change it to door??    or recreate the constructor
     door.open();
     engine.events.create_event<UpdateFOV>();
+    engine.events.create_event<Sound>("door-open");
     return success();
 }
 
