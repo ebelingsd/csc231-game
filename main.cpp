5fa913f098577ddb708905d426c20ad21bb80e2b
silas <silasebeling8@gmail.com>
2024-04-28 19:36:37 -0700

    modify numbers of monsters

--- main.cpp
+++ main.cpp
@@ -11,15 +11,15 @@ int main() {
         std::shared_ptr<Entity> hero = engine.create_hero();
         Heroes::make_knight(hero);
 
-        for (int i = 0; i < 20; ++i) {
+        for (int i = 0; i < 15; ++i) {
             std::shared_ptr<Entity> monster = engine.create_monster();
             Monsters::make_orc_masked(monster);
         }
-        for (int i = 0; i < 20; ++i) {
+        for (int i = 0; i < 10; ++i) {
             std::shared_ptr<Entity> monster = engine.create_monster();
             Monsters::make_ogre(monster);
         }
-        for (int i = 0; i < 20; ++i) {
+        for (int i = 0; i < 15; ++i) {
             std::shared_ptr<Entity> monster = engine.create_monster();
             Monsters::make_goblin(monster);
         }
