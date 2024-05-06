17e8f3321558e5421858e3005f9fd32fef262847
silas <silasebeling8@gmail.com>
2024-05-06 16:13:01 -0700

    update die to include drop

--- content/events/die.cpp
+++ content/events/die.cpp
@@ -1,10 +1,15 @@
 #include "die.h"
 #include "engine.h"
 #include "entity.h"
+#include "drop.h"
+
 
 Die::Die(Entity& entity)
     :entity{entity} {}
 
 void Die::execute(Engine& engine) {
     engine.remove_entity(entity);
+    //drops the weapon when the monster dies
+    engine.events.create_event<Drop>(entity);
 }
+
